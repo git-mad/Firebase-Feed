bc093be4055e1910933f1453b623f9852633b3cc
Alex Bettadapur <alexbettadapur@gmail.com>
2015-03-03 17:04:15 -0500

    Firebase interface

---
+++ app/src/main/java/org/gitmad/firebasefeed/firebase/IFirebaseSource.java
@@ -0,0 +1,19 @@
+package org.gitmad.firebasefeed.firebase;
+
+import org.gitmad.firebasefeed.models.Post;
+import org.gitmad.firebasefeed.models.User;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 3/3/2015.
+ */
+public interface IFirebaseSource
+{
+    void AddPost(Post post);
+    List<Post> listPosts();
+    Post getPost(int id);
+
+    User getUser(int id);
+
+}
